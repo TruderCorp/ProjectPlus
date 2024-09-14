9ad7bc0c5207148923db0e01818d6ac8b23e6b8b
trudercorp <167313975+TruderCorp@users.noreply.github.com>
2024-09-14 15:46:27 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const { Server: SocketServer } = require("socket.io");
+
+const app = express();
+
+app.use("/", express.static("public"));
+
+const httpServer = app.listen(8080, () => {
+  console.log(`Server started on 3000`);
+});
+
+const io = new SocketServer(httpServer);
+io.on("connection", (socket) => {
+  console.log("new connection from ", socket.id);
+
+  socket.on("offer", (offer) => {
+    console.log("new offer from ", socket.id);
+    socket.broadcast.emit("offer", offer);
+  });
+
+  socket.on("answer", (answer) => {
+    console.log("new answer from ", socket.id);
+    socket.broadcast.emit("answer", answer);
+  });
+
+  socket.on("icecandidate", (candidate) => {
+    console.log("new ice candidate from ", socket.id);
+    socket.broadcast.emit("icecandidate", candidate);
+  });
+});
---
+++ public/admin.js
@@ -0,0 +1,28 @@
+"use strict";
+const socket = io();
+const configuration = {
+  iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+};
+const peer = new RTCPeerConnection(configuration);
+
+const video = document.getElementById("client-screen");
+peer.addEventListener("track", (track) => {
+  video.srcObject = track.streams[0];
+});
+
+socket.on("offer", async (clientSDP) => {
+  await peer.setRemoteDescription(clientSDP);
+
+  const sdp = await peer.createAnswer();
+  await peer.setLocalDescription(sdp);
+  socket.emit("answer", peer.localDescription);
+});
+
+peer.addEventListener("icecandidate", (event) => {
+  if (event.candidate) {
+    socket.emit("icecandidate", event.candidate);
+  }
+});
+socket.on("icecandidate", async (candidate) => {
+  await peer.addIceCandidate(new RTCIceCandidate(candidate));
+});
